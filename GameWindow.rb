cfcdc399582589760aaccaff240bf8338d563089
Raven <RavensKrag@gmail.com>
2012-09-20 17:01:14 -0400

    Set up basic text.

--- GameWindow.rb
+++ GameWindow.rb
@@ -9,15 +9,37 @@ class GameWindow < Gosu::Window
 		super(740, 470, false, (1.0/@target_fps)*1000) # Same size as the pane of the original comic
 		self.caption = "xkcd: Click and Drag "
 		
+		@font = Gosu::Font.new self, "Trebuchet MS", 34
+		
 		# Stream resources from xkcd website
 		# This program essentially serves as a viewer for the Click and Drag comment, with
 		# some modifications.
+		
+		# Recreate the opening panels of the comic, so that they can be animated more throughly
+	end
+	
 	def update
 		
 	end
 	
 	def draw
+		self.translate 0,0 do
+			@font.draw "From the stories",0,0,0
+			@font.draw "I expected the / world to be sad",0,0,0
+			@font.draw "and it was",0,0,0
+		end
 		
+		self.translate 200,0 do
+			@font.draw "and I expected it / to be wonderful.",0,0,0
+		end
+		
+		self.translate 400,0 do
+			@font.draw "it was.",0,0,0
+		end
+		
+		self.translate 50, 200 do
+			@font.draw "I just didn't expect / it to be so BIG",0,0,0
+		end
 	end
 	
 	def button_down(id)
