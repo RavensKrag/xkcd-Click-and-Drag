97a7861238959d45a0fe029177dbd854cb64a077
Raven <RavensKrag@gmail.com>
2012-09-20 17:38:53 -0400

    Attempt typesetting of first panel.

--- GameWindow.rb
+++ GameWindow.rb
@@ -9,7 +9,7 @@ class GameWindow < Gosu::Window
 		super(740, 470, false, (1.0/@target_fps)*1000) # Same size as the pane of the original comic
 		self.caption = "xkcd: Click and Drag "
 		
-		@font = Gosu::Font.new self, "Trebuchet MS", 34
+		@font = Gosu::Font.new self, "Trebuchet MS", 25
 		
 		# Stream resources from xkcd website
 		# This program essentially serves as a viewer for the Click and Drag comment, with
@@ -24,22 +24,30 @@ class GameWindow < Gosu::Window
 	
 	def draw
 		self.translate 0,0 do
-			@font.draw "From the stories",0,0,0
-			@font.draw "I expected the / world to be sad",0,0,0
-			@font.draw "and it was",0,0,0
+			draw_text "From the stories", 0,0,0
+			draw_text ["I expected the",
+					"world to be sad"], 30,@font.height,0
+			draw_text "and it was", 30, 100,0
+			
+			#~ draw_text do
+				#~ puts "From the stories"
+				#~ puts "I expected the"
+				#~ puts "world to be sad"
+				#~ puts "and it was"
+			#~ end
 		end
-		
-		self.translate 200,0 do
-			@font.draw "and I expected it / to be wonderful.",0,0,0
-		end
-		
-		self.translate 400,0 do
-			@font.draw "it was.",0,0,0
-		end
-		
-		self.translate 50, 200 do
-			@font.draw "I just didn't expect / it to be so BIG",0,0,0
-		end
+		#~ 
+		#~ self.translate 200,0 do
+			#~ @font.draw "and I expected it / to be wonderful.",0,0,0
+		#~ end
+		#~ 
+		#~ self.translate 400,0 do
+			#~ @font.draw "it was.",0,0,0
+		#~ end
+		#~ 
+		#~ self.translate 50, 200 do
+			#~ @font.draw "I just didn't expect / it to be so BIG",0,0,0
+		#~ end
 	end
 	
 	def button_down(id)
@@ -51,5 +59,22 @@ class GameWindow < Gosu::Window
 	
 	def button_up(id)
 		
+	end
+	
+	private
+	
+	# Render text with newlines
+	def draw_text(lines, x=0, y=0, z=0)
+		#~ lines = string.split("\n")
+		if lines.respond_to? :each_with_index
+			self.translate x,y do
+				lines.each_with_index do |l, i|
+					@font.draw l, 0, i*@font.height, z
+				end
+			end
+		else
+			# Drawing only one line
+			@font.draw lines, x, y, z
+		end
 	end
 end
